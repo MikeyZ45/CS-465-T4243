4154b43282a798b07e653ed561a77c3a78be0503
Mikey <michael.divis@snhu.edu>
2023-04-20 21:45:44 -0700

    Commit all files except app_admin

---
+++ app_server/controllers/travel.js
@@ -0,0 +1,101 @@
+// var fs = require('fs');
+
+// var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+const request = require('request');
+const apiOptions = {
+ server: 'http://localhost:3000'
+}
+
+
+
+
+
+// GET travler list view
+const renderTravelList = (req, res, responseBody) => {
+    let message = null;
+    let pageTitle = process.env.npm_package_description + ' - Travel ';
+
+    if (!(responseBody instanceof Array)) {
+        message = 'API lookup error';
+        responseBody = [];
+    } else {
+        if (!responseBody.length) {
+            message = "no trips exist in database!";
+        }
+    }
+
+    res.render('travel', {
+        title: pageTitle,
+        trips: responseBody,
+        message
+    });
+};
+
+
+// GET travler list view
+const travelList = (req, res) => {
+    const path = '/api/trips';
+    const requestOptions = {
+        url:    `${apiOptions.server}${path}`,
+        method: 'GET',
+        json: {},
+    };
+
+    console.info('>> travelController.travelList calling ' + requestOptions.url);
+
+    request(
+        requestOptions,
+        (err, { statusCode }, body) => {
+            if (err) {
+                console.error(err);
+            }
+            renderTravelList(req, res, body);
+        }
+    )
+}
+
+module.exports = {
+    travelList
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /* GET travel view */
+// const travel = (req, res) => {
+//     res.render('travel', { title: 'Travlr Getaways', trips });
+// };
+// module.exports = {
+//     travel
+// };
+   
